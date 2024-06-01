53469d25eaa5d5fc536bcc61ff63879f6e747118
Buse KURT <busekurt86@gmail..com>
2024-06-01 19:49:53 +0300

    update

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import LabelEncoder
 
 # Load the dataset
