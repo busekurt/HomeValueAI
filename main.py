f6b79a8bf9e18b4b194e81497d3228cc4cd9e7a3
Buse KURT <busekurt86@gmail..com>
2024-06-01 20:08:32 +0300

    update2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import LabelEncoder
 
 # Load the dataset
@@ -35,6 +36,10 @@ model.fit(X_train, y_train)
 
 # Make predictions
 y_pred = model.predict(X_test)
+
+# Evaluate the model's performance
+mse = mean_squared_error(y_test, y_pred)
+print(f'Mean Squared Error: {mse}')
 
 # Features of a new house
 new_house = {
